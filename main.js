935bd85c14edb78ad47f008d58b7690c3aab501e
Grant-Howard509 <howardgrant120@gmail.com>
2024-09-26 13:16:45 -0700

    Big update
    CSS color variables have been added to the root of the document. JS file now changes CSS variable values depending on whether or not a section is intersecting with the viewport. Left footer section is now implemented with styling.

--- main.js
+++ main.js
@@ -1,41 +1,21 @@
 const issues = Array.from(document.querySelectorAll('.issue-section'));
 
+function handleIntersection(target) {
+    document.documentElement.style.setProperty(`--base`, `var(--${target.id}-color)`);
+
+    if (target.id == 'issue6') {
+        document.documentElement.style.setProperty(`--anchor`, `var(--anchor-active)`);
+    } else {
+        document.documentElement.style.setProperty(`--anchor`, `#fff`);
+    }
+}
+
 function changeColor(entries) {
     entries.forEach(entry => {
         const intersecting = entry.isIntersecting;
         
         if (intersecting) {
-            let currentID = entry.target.id;
-            const bg = document.querySelector('body').style;
-    
-            switch (currentID) {
-                case 'issue8':
-                    bg.backgroundColor = '#f6e0a4';
-                    break;
-                case 'issue7':
-                    bg.backgroundColor = '#ff608c';
-                    break;
-                case 'issue6':
-                    bg.backgroundColor = '#fff';
-                    break;
-                case 'issue5':
-                    bg.backgroundColor = '#00c1b5';
-                    break;
-                case 'issue4':
-                    bg.backgroundColor = '#ff6519';
-                    break;
-                case 'issue3':
-                    bg.backgroundColor = '#ffbe00';
-                    break;
-                case 'issue2':
-                    bg.backgroundColor = '#1d3fbb';
-                    break;
-                case 'issue1':
-                    bg.backgroundColor = '#E30512';
-                    break;
-                default:
-                    bg.backgroundColor = '#000';
-            }
+            handleIntersection(entry.target);
         }
     });
 }
